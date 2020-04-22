d5e7d028e040bffde961ec7e0fb6b99a074bf002
maxmakovskiy <makovskiymax@gmail.com>
2020-04-23 00:41:05 +0300

    Completed -> sum_of_positive

--- sum_of_positive/sum.go
+++ sum_of_positive/sum.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-func PositiveSum(number []int) int {
+func PositiveSum(numbers []int) int {
+	var res int
 
+	for _, v := range numbers {
+		if v > 0 {
+			res += v
+		}
+	}
+
+	return res
 }
 
 func main() {
-
-	fmt.Println("HELLO")
+	nums := []int{-1, 2, 3, 4, -5}
+	fmt.Printf("SOURCE: %#v\nRESULT: %d", nums, PositiveSum(nums))
 }
