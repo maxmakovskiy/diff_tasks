dc4cf4595f05d137cacf7206cdc9a0d31b3ffe09
maxmakovskiy <makovskiymax@gmail.com>
2020-04-25 12:04:55 +0300

    Completed -> deadfish_swim

--- deadfish_swim/swimz.go
+++ deadfish_swim/swimz.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
+// "i" - incr value
+// "d" - decr value
+// "s" - sqr value
+// "o" - output value
+// "iiisdoso" -> []int{8, 64}
 func Parse(data string) []int {
+	res := make([]int, 0)
+	var value int
+	for _, k := range strings.Split(data, "") {
+		if t := strings.Compare(k, "i"); t == 0 {
+			value++
+		} else if t := strings.Compare(k, "d"); t == 0 {
+			value--
+		} else if t := strings.Compare(k, "s"); t == 0 {
+			value = (value * value)
+		} else if t := strings.Compare(k, "o"); t == 0 {
+			res = append(res, value)
+		}
+	}
 
-	return []int{math.Pow(2, 1)}
+	return res
 }
 
 func main() {
-	fmt.Println("INIT")
+	src := "iiisdoso"
+	fmt.Printf("init value -> %s\nresult -> %v\n", src, Parse(src))
 }
