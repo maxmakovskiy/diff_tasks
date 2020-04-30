f0b621ef25825ae7c81e1b5a8df501004f610843
maxmakovskiy <makovskiymax@gmail.com>
2020-04-30 21:33:47 +0300

    ="

---
+++ string_patterns/string_patterns/src/main.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+// return index of t in s, -1 if none
+int strindex(char s[], char t[])
+{
+	int i, j, k;
+	
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		for (j = i, k = 0; (t[k] = '\0') && (s[j] == t[k]); j++, k++);
+		
+		if ((k > 0) && (t[k] == '\0'))
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+// get input (line) to s array and return length of s
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while (--lim > 0 && ((c = getchar() != EOF) && (c != '\n')))
+	{
+		s[i++] = c;
+	}
+
+	if (c == '\n')
+	{
+		s[i++] = c;
+	}
+
+	s[i] = '\0';
+
+	return i;
+}
+
+int main() 
+{
+	char pattern[] = "ello";
+	char line[MAXLINE];
+	int found = 0;
+
+	while (getline(line, MAXLINE) > 0)
+	{
+		if (strindex(line, pattern) >= 0)
+		{
+			printf("%s", line);
+		}
+	}
+
+	printf("FOUND: %d\n", found);
+
+
+	return 0;
+}
