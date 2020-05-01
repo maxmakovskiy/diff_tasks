b43b02e8cfc7942e47be84179d40bc2ae77211f5
maxmakovskiy <makovskiymax@gmail.com>
2020-05-01 15:21:18 +0300

    Completed -> countLinesTabsBlanks

--- character_input_output/character_input_output/src/main.cpp
+++ character_input_output/character_input_output/src/main.cpp
@@ -22,6 +22,30 @@ int countCharacters()
 	return numOfChars;
 }
 
+int* countLinesBlanksTabs()
+{
+	static int storage[3] = { 0, 0, 0 };
+	int c;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\n')
+		{
+			storage[0]++;
+		}
+		else if (c == '\t')
+		{
+			storage[1]++;
+		}
+		else if (c == ' ')
+		{
+			storage[2]++;
+		}
+	}
+	
+	return storage;
+}
+
 int countLines()
 {
 	int numOfLines = 0;
@@ -46,9 +70,14 @@ int main()
 /*	int count = countCharacters();
 	printf("Number of characters from input: %d", count); */
 
-	int count = countLines();
+	/*int count = countLines();
 	printf("Num of lines from given input: %d\n", count);
-	getchar();
+	getchar();*/
+
+	int* counterp = countLinesBlanksTabs();
+	printf("Num of lines %d\nNum of tabs %d\nNum of blanks %d\n", *counterp, *(counterp + 1), *(counterp + 2));
+
+
 
 	system("pause");
 	return 0;
