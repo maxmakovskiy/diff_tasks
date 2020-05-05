1ac5350a40f346c5c0da241bc6e82c96e706497b
maxmakovskiy <makovskiymax@gmail.com>
2020-05-05 10:54:43 +0300

    Fixed -> algorithm of Leibniz Formula

--- PI_approximation/approximation.go
+++ PI_approximation/approximation.go
@@ -6,22 +6,25 @@ import (
 )
 
 func seriesOfLeibnizAt(n int) (result float64) {
-	for i := 0; i < n; i++ {
-		result += math.Pow(-1.0, float64(i)) / float64((2*n)+1)
+	for i := 0; i <= n; i++ {
+		temp := math.Pow(-1.0, float64(i)) / float64((2*i)+1)
+		result += temp
 	}
 
 	return
 }
 
 func IterPi(epsilon float64) (int, string) {
-	var n int
+	var n int = 1
 	var iterationCounter int
 	for {
-		if t := 4 * seriesOfLeibnizAt(n); epsilon > (math.Pi - t) {
-			s := strconv.FormatFloat(t, 'f', -1, 64)
+		t := 4.0 * seriesOfLeibnizAt(n)
+		if epsilon > math.Abs(math.Pi-t) {
+			s := strconv.FormatFloat(t, 'f', 10, 64)
+			//			s := fmt.Sprintf("%.10f", t)
 			return iterationCounter, s
 		}
-		n += 4
+		n++
 		iterationCounter++
 	}
 }
